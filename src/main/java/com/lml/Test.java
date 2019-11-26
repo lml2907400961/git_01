88dd1ed5f6fc90be92c0762f0dfe53f041220fca
lml2907400961 <2907400961@qq.com>
2019-11-26 16:03:51 +0800

    克隆git_01后修改的文件提交代码

--- src/main/java/com/lml/Test.java
+++ src/main/java/com/lml/Test.java
@@ -3,7 +3,7 @@ package com.lml;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello lml");
-        System.out.println("修改后上转的代码");
+        System.out.println("hello Clone lml");
+        System.out.println("这是克隆Git_01的文件");
     }
 }
